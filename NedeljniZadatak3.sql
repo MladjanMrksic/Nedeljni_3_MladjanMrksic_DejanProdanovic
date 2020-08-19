606f7edb57a395e5f466655ddac28f30d21a8c3b
Dalish <Dalish@DESKTOP-S7S9PJT>
2020-08-19 14:34:31 +0200

    Added DLL SQL script

---
+++ NedeljniZadatak3.sql
@@ -0,0 +1,53 @@
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'CookbookDatabase')
+CREATE DATABASE CookbookDatabase
+GO
+
+USE CookbookDatabase
+GO
+
+IF EXISTS (SELECT name FROM sys.sysobjects WHERE name = 'tblIngredients')
+drop table tblIngredients
+IF EXISTS (SELECT name FROM sys.sysobjects WHERE name = 'tblShoppingList')
+drop table tblShoppingList
+IF EXISTS (SELECT name FROM sys.sysobjects WHERE name = 'tblRecipe')
+drop table tblRecipe
+IF EXISTS (SELECT name FROM sys.sysobjects WHERE name = 'tblPerson')
+drop table tblPerson
+
+CREATE TABLE tblPerson
+(
+PersonID int primary key identity(1,1),
+FirstName nvarchar(50),
+LastName nvarchar(50),
+Username nvarchar(50) unique,
+Password nvarchar(50) CHECK (LEN(Password) > 5 ),
+)
+
+CREATE TABLE tblRecipe 
+(
+RecipeID int primary key identity(1,1),
+RecipeName nvarchar(50),
+RecipeType nvarchar(20) CHECK (UPPER(RecipeType) = 'APPETIZER' or UPPER(RecipeType) = 'MEAL' or UPPER(RecipeType) = 'Dessert'),
+IntendedFor int,
+Author int FOREIGN KEY REFERENCES tblPerson(PersonID),
+Description nvarchar(150),
+DateCreated date
+)
+
+CREATE TABLE tblIngredients
+(
+IngredientID int primary key identity (1,1),
+IngredientName nvarchar(50),
+Ammount int,
+Recipe int FOREIGN KEY REFERENCES tblRecipe(RecipeID),
+)
+
+CREATE TABLE tblShoppingList
+(
+IngredientID int primary key identity (1,1),
+IngredientName nvarchar(50),
+Ammount int,
+Owned bit default(0),
+Recipe int FOREIGN KEY REFERENCES tblRecipe(RecipeID),
+Person int FOREIGN KEY REFERENCES tblPerson(PersonID)
+)
